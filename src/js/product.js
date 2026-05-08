2a42c89b481c250ed3119b6e8f33e8f03369d141
marthagram <mrsmartha.otas@gmail.com>
2026-05-08 23:23:35 +0100

    fixed cart logic of items addition in it

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const products = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+
+  let found = products.find((item) => item.Id === product.Id); // check if product is already in cart
+  if (found) {
+    found.quantity += 1; // if product is already in cart increase quantity by 1
+  } else {
+    products.push(product);
+  }
+  setLocalStorage("so-cart", products);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
