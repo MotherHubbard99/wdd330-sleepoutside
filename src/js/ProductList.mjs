02609e738f3c467e158e04caaf1f561fa2765c9d
marthagram <mrsmartha.otas@gmail.com>
2026-05-20 10:02:57 +0100

    individual tasks development

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,14 +1,29 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
-  return `<li class="product-card">
-    <a href="product_pages/?product=${product.Id}">
-      <img src="${product.Image}" alt="Image of ${product.Name}">
-      <h2 class="card__brand">${product.Brand}</h2>
-      <h3 class="card__name">${product.Name}</h3>
-      <p class="product-card__price">$${product.FinalPrice.toFixed(2)}</p>
-    </a>
-  </li>`
+   
+      if (product.FinalPrice < product.SuggestedRetailPrice){
+        const discount = product.SuggestedRetailPrice - product.FinalPrice;
+
+        return `<li class="product-card">
+          <a href="product_pages/?product=${product.Id}">
+            <img src="${product.Image}" alt="Image of ${product.Name}">
+            <h2 class="card__brand">${product.Brand}</h2>
+            <h3 class="card__name">${product.Name}</h3>
+            <p>discounted price: $${discount.toFixed(2)}</p>
+            <p class="product-card__price">$${product.FinalPrice.toFixed(2)}</p>
+          </a>
+        </li>`;
+      } else {
+        return `<li class="product-card">
+          <a href="product_pages/?product=${product.Id}">
+            <img src="${product.Image}" alt="Image of ${product.Name}">
+            <h2 class="card__brand">${product.Brand}</h2>
+            <h3 class="card__name">${product.Name}</h3>
+            <p class="product-card__price">$${product.FinalPrice.toFixed(2)}</p>
+          </a>
+        </li>`;
+      }
 }
 
 export default class ProductList {
