c1436c8070263a22728f264de12c76fbcd68bd8b
MotherHubbard99 <122504080+MotherHubbard99@users.noreply.github.com>
2026-05-27 08:31:27 -0600

    Revert "Gmm  individual3"

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,22 +1,16 @@
-
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { loadHeaderFooter, getParam } from "./utils.mjs";
 
 loadHeaderFooter();
 
-// We extract the category parameter dynamically from the browser URL address bar
-// If a user clicks 'Backpacks', this automatically grabs "backpacks" from ?category=backpacks
 const category = getParam("category");
-
-// We then initialize the updated data source class
+//create an instance of the ProductData class
 const dataSource = new ProductData();
+//get the element from the product list that you want to render
+const element = document.querySelector(".product-list");
+//create an instance of the ProductList class and send it the correct informatio
+const listing = new ProductList(category, dataSource, element);
+//call the init method to render the product list
 
-// We then grab the container element from the DOM where the list cards will inject
-const listElement = document.querySelector(".product-list");
-
-// We then Instantiate the ProductList renderer component, passing the category and source down 
-const myList = new ProductList(category, dataSource, listElement);
-
-// We then run the initialization to fetch from the API and build the HTML templates
-myList.init();
+listing.init();
