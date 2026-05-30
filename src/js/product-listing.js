9f471de5a599aa2e522980fb457062fc9b8e917e
ssaliarafat <marvelarafat@gmail.com>
2026-05-30 11:58:29 +0300

    Add customizable alert to index.html

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,21 +1,29 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { getParam } from "./utils.mjs";
+import alerts from "../json/alerts.json";
 
 const category = getParam("category") || "tents";
-
 const dataSource = new ProductData();
-
 const listElement = document.querySelector(".product-list");
-
 const titleElement = document.querySelector(".title");
+const alertsElement = document.querySelector(".alerts");
 
 if (titleElement) {
-  titleElement.textContent =
-    `Top Products: ${category.replace("-", " ")}`;
+  titleElement.textContent = `Top Products: ${category.replace("-", " ")}`;
 }
 
-const productList =
-  new ProductList(category, dataSource, listElement);
+if (alertsElement && Array.isArray(alerts)) {
+  alertsElement.innerHTML = alerts.map(alertTemplate).join("");
+}
 
+const productList = new ProductList(category, dataSource, listElement);
 productList.init();
+
+function alertTemplate(alert) {
+  return `
+    <div class="alert-banner" style="background-color: ${alert.background};">
+      <p>${alert.message}</p>
+    </div>
+  `;
+}
