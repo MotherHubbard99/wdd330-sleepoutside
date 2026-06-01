bdd408847c111c70c74075846c1bc30407655ed9
Garikai88 <gmangobe@byupathway.edu>
2026-06-01 14:43:42 +0200

    feat: implement register cta modal and final proposal documentation.

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,38 @@ import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
+export function initWelcomeModal() {
+    const modal = document.getElementById("welcome-modal");
+    const closeBtn = document.getElementById("close-modal-btn");
+
+    // Check localStorage to see if the user has visited before
+    const hasVisited = localStorage.getItem("visited-sleep-outside");
+
+    if (!hasVisited) {
+        // If no recored exists, remove the 'hidden' class t show the banner modal
+        modal.classList.remove("hidden");
+
+        // We immediately set the key in localStorage so it won't appear on subsequent reloads
+        localStorage.setItem("visited-sleep-outside", "true");
+
+        // Handle manual close click event
+        if (closeBtn && modal) {
+            closeBtn.addEventListener("click", () => {
+                modal.classList.add("hidden");
+            });
+
+            // Optional: Close the modal if they click anywhere on the dark background overlay
+            window.addEventListener("click", (event) => {
+                if (event.target === modal) {
+                    modal.classList.add("hidden");
+                }
+            });
+        }
+
+
+    }
+}
+
+// We then invoke the check on page DOM content load
+document.addEventListener("DOMContentLoaded", initWelcomeModal);
+
