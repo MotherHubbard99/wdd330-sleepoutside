1920b44d9c0b8d834569d6cde3b4b302d0881a6b
Garikai88 <gmangobe@byupathway.edu>
2026-06-01 17:20:17 +0200

    fix: clear raw git merge conflict markers from root vite config

--- vite.config.js
+++ vite.config.js
@@ -13,11 +13,9 @@ export default defineConfig({
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
-<<<<<<< HEAD
+
         listing: resolve(__dirname, "src/product_listing/index.html"),
-=======
         product_listing: resolve(__dirname, "src/product_listing/index.html"),
->>>>>>> origin/main
       },
     },
   },
